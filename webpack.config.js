80be55797cec0902393551d51f976b231a66da55
Kvkens <kvkens@qq.com>
2019-10-11 16:29:54 +0800

    fix:build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');  // 导出额外的文件插件
+var ExtractTextPlugin = require('extract-text-webpack-plugin'); // 导出额外的文件插件
 var StringReplacePlugin = require('string-replace-webpack-plugin'); // 字符串替换插件
 
 // 获取环境变量, 方便做不同处理
@@ -16,8 +16,8 @@ var cssmode = package.cssmode;
 
 // 输出口
 var output = {
-    path: path.resolve(__dirname, "build/assets"),
-    filename: '[name].js' 
+    filename: '[name].js',
+    path: __dirname + '/dist'
 };
 
 // 声明cssloader
@@ -27,28 +27,26 @@ var cssLoader = {
 };
 
 // 为product环境打包时
-if (env == 'product') {
-    // 定制cdn路径
-    output.publicPath = 'http://h.cdn.pengpengla.com/' + projectName + '/' + projectVersion + '/assets/';
-    cssLoader.loader = ExtractTextPlugin.extract("style-loader", "css-loader");
-}
+// if (env == 'product') {
+//     // 定制cdn路径
+//     output.publicPath = 'http://h.cdn.pengpengla.com/' + projectName + '/' + projectVersion + '/assets/';
+//     cssLoader.loader = ExtractTextPlugin.extract("style-loader", "css-loader");
+// }
 
-if (env == 'stage') {
-     // 定制cdn路径
-    output.publicPath = '/' + projectName + '/' + projectVersion + '/assets/';
-    cssLoader.loader = ExtractTextPlugin.extract("style-loader", "css-loader");
-}
+// if (env == 'stage') {
+//      // 定制cdn路径
+//     output.publicPath = '/' + projectName + '/' + projectVersion + '/assets/';
+//     cssLoader.loader = ExtractTextPlugin.extract("style-loader", "css-loader");
+// }
 
 module.exports = {
     entry: {
-        // 可对应多个入口文件
-        test: './test.js'
+        index: './src/index.js'
     },
     output: output,
-    devtool: 'source-map', // 输出source-map
-    module: {
-        loaders: [
-            {
+    // devtool: 'source-map', // 输出source-map
+    module: {
+        loaders: [{
                 test: /\.jsx?$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/,
@@ -56,43 +54,41 @@ module.exports = {
                     presets: ['es2015', 'react', 'stage-0']
                 }
             },
-            
+
             cssLoader,
-            { 
+            {
                 test: /\.css|jsx?$/,
                 loader: StringReplacePlugin.replace({
-                    replacements: [
-                        {
-                            pattern: /\d+?px['"; ]/ig,
-                            replacement: function (res) {
-                                // 若cssmode为0, 则不需要额处理单位
-                                // 可选: 640 | 750
-                                if (cssmode > 0) {
-                                    res = res.replace(/(\d+?)px([; ',"])/ig, function($1 , $2 , $3, index , source) {
-                                        return ($2 * 2) / (cssmode / 10) + 'rem' + $3;
-                                    });
-                                }
-                                return res;
+                    replacements: [{
+                        pattern: /\d+?px['"; ]/ig,
+                        replacement: function (res) {
+                            // 若cssmode为0, 则不需要额处理单位
+                            // 可选: 640 | 750
+                            if (cssmode > 0) {
+                                res = res.replace(/(\d+?)px([; ',"])/ig, function ($1, $2, $3, index, source) {
+                                    return ($2 * 2) / (cssmode / 10) + 'rem' + $3;
+                                });
                             }
+                            return res;
                         }
-                    ]
+                    }]
                 })
             },
-            { 
-                test: /\.png$/, 
+            {
+                test: /\.png$/,
                 loader: "url-loader?limit=10000" // 小于3k, 转成base64
             },
-            { 
-                test: /\.jpg$/, 
-                loader: "file-loader" 
+            {
+                test: /\.jpg$/,
+                loader: "file-loader"
             }
         ]
-    },
+    },
     // server配置
     // sudo webpack-dev-server
     devServer: {
-        contentBase: './',  // 服务根目录
-        color: true,  // 命令行是否彩色
+        contentBase: './', // 服务根目录
+        color: true, // 命令行是否彩色
         inline: true, // 项目文件保存自动编译文件模块
         port: 80 // 启动端口
     },
